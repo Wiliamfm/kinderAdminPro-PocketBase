d4cc9bae9ff6108471fae8006bde4da23594810f
william <santifon15@gmail.com>
2026-03-01 17:55:31 -0500

    Create bulletins_students collection

---
+++ pb_migrations/1772405587_created_bulletins_students.js
@@ -0,0 +1,170 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = new Collection({
+    "createRule": "@request.auth.is_admin = true",
+    "deleteRule": "@request.auth.is_admin = true",
+    "fields": [
+      {
+        "autogeneratePattern": "[a-z0-9]{15}",
+        "hidden": false,
+        "id": "text3208210256",
+        "max": 15,
+        "min": 15,
+        "name": "id",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": true,
+        "required": true,
+        "system": true,
+        "type": "text"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "pbc_3889984678",
+        "hidden": false,
+        "id": "relation3517624886",
+        "maxSelect": 1,
+        "minSelect": 1,
+        "name": "bulletin_id",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "pbc_3827815851",
+        "hidden": false,
+        "id": "relation3415494426",
+        "maxSelect": 1,
+        "minSelect": 1,
+        "name": "student_id",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "pbc_233839710",
+        "hidden": false,
+        "id": "relation4263092648",
+        "maxSelect": 1,
+        "minSelect": 1,
+        "name": "grade_id",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "pbc_2708049880",
+        "hidden": false,
+        "id": "relation1249479535",
+        "maxSelect": 1,
+        "minSelect": 1,
+        "name": "semester_id",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "hidden": false,
+        "id": "number3485334036",
+        "max": 10000,
+        "min": 1,
+        "name": "note",
+        "onlyInt": true,
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "number"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text1604228650",
+        "max": 0,
+        "min": 0,
+        "name": "comments",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": false,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "_pb_users_auth_",
+        "hidden": false,
+        "id": "relation3725765462",
+        "maxSelect": 1,
+        "minSelect": 1,
+        "name": "created_by",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "_pb_users_auth_",
+        "hidden": false,
+        "id": "relation385774305",
+        "maxSelect": 1,
+        "minSelect": 1,
+        "name": "updated_by",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "hidden": false,
+        "id": "autodate2341372968",
+        "name": "created_at",
+        "onCreate": true,
+        "onUpdate": false,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      },
+      {
+        "hidden": false,
+        "id": "autodate1130519967",
+        "name": "updated_at",
+        "onCreate": true,
+        "onUpdate": true,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      },
+      {
+        "hidden": false,
+        "id": "bool4245145851",
+        "name": "is_deleted",
+        "presentable": false,
+        "required": false,
+        "system": false,
+        "type": "bool"
+      }
+    ],
+    "id": "pbc_1378873045",
+    "indexes": [],
+    "listRule": "@request.auth.is_admin = true",
+    "name": "bulletins_students",
+    "system": false,
+    "type": "base",
+    "updateRule": "@request.auth.is_admin = true",
+    "viewRule": "@request.auth.is_admin = true"
+  });
+
+  return app.save(collection);
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1378873045");
+
+  return app.delete(collection);
+})
