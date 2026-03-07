28ab3da0e9e0e9effc34b0791e07d66f34024454
william <santifon15@gmail.com>
2026-03-07 05:28:37 -0500

    Add employee_reports collection

---
+++ pb_migrations/1772879162_created_employee_reports.js
@@ -0,0 +1,145 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = new Collection({
+    "createRule": "@request.auth.is_admin = true",
+    "deleteRule": "@request.auth.is_admin = true",
+    "fields": [
+      {
+        "autogeneratePattern": "[a-z0-9]{15}",
+        "hidden": false,
+        "id": "text3208210256",
+        "max": 15,
+        "min": 15,
+        "name": "id",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": true,
+        "required": true,
+        "system": true,
+        "type": "text"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "pbc_3735627160",
+        "hidden": false,
+        "id": "relation2349068636",
+        "maxSelect": 1,
+        "minSelect": 1,
+        "name": "employee_id",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "pbc_4101905139",
+        "hidden": false,
+        "id": "relation199249577",
+        "maxSelect": 1,
+        "minSelect": 1,
+        "name": "job_id",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "pbc_2708049880",
+        "hidden": false,
+        "id": "relation1249479535",
+        "maxSelect": 1,
+        "minSelect": 1,
+        "name": "semester_id",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text1604228650",
+        "max": 0,
+        "min": 0,
+        "name": "comments",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": false,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "_pb_users_auth_",
+        "hidden": false,
+        "id": "relation3725765462",
+        "maxSelect": 1,
+        "minSelect": 1,
+        "name": "created_by",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "_pb_users_auth_",
+        "hidden": false,
+        "id": "relation385774305",
+        "maxSelect": 1,
+        "minSelect": 1,
+        "name": "updated_by",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "hidden": false,
+        "id": "autodate2341372968",
+        "name": "created_at",
+        "onCreate": true,
+        "onUpdate": false,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      },
+      {
+        "hidden": false,
+        "id": "autodate1130519967",
+        "name": "updated_at",
+        "onCreate": true,
+        "onUpdate": true,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      },
+      {
+        "hidden": false,
+        "id": "bool4245145851",
+        "name": "is_deleted",
+        "presentable": false,
+        "required": false,
+        "system": false,
+        "type": "bool"
+      }
+    ],
+    "id": "pbc_1866223567",
+    "indexes": [],
+    "listRule": "@request.auth.is_admin = true",
+    "name": "employee_reports",
+    "system": false,
+    "type": "base",
+    "updateRule": "@request.auth.is_admin = true",
+    "viewRule": "@request.auth.is_admin = true"
+  });
+
+  return app.save(collection);
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1866223567");
+
+  return app.delete(collection);
+})
